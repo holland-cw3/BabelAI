9f69aeb2373c2f446107da7c22ccd44e144f6ed0
Caleb Holland <hollandcw3@gmail.com>
2026-02-01 13:34:23 -0500

    voice command uno

--- backend/server.py
+++ backend/server.py
@@ -1,25 +1,58 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 from dotenv import load_dotenv
+import os
+import subprocess
+import platform
+
+system = platform.system()
+username = os.getlogin()
+folder_path = fr"C:\Users\{username}"
+
 
 load_dotenv()
 
-class Login(Resource):
+class Command(Resource):
     def post(self):
-        # data = request.get_json()
-        # field = data.get("field")        
+        data = request.get_json()
+
+
+        command = data.get('command')
+        command = command.replace('.', '')
+        command = command.replace(',', '')
+        command = command.replace(';', '')
+
+
+        print(command)
         
-        return {'message': 'message', 'blah': 'blah'}, 200
-       
-  
-# ----- ###########   Restful API   ########### ----- #
+
+        if command.lower() == 'open home':
+            if system == 'Windows':
+                subprocess.run([
+                    'powershell', 
+                    '-Command',
+                     f'Start-Process "{folder_path}" -WindowStyle Normal'
+                ])
+
+        if command.lower() == 'open cmsc 414':
+            if system == 'Windows':
+                subprocess.run([
+                    'powershell', 
+                    '-Command',
+                     f'Start-Process "{folder_path + '/CMSC414'}" -WindowStyle Normal'
+                ])
+           
+        return {'message': 'success'}, 200
+    
+
+# # ----- ###########   Restful API   ########### ----- #
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
  
-api.add_resource(Login, "/Login")
+api.add_resource(Command, "/command")
 
 
 if __name__ == "__main__":
--- frontend/src/components/voice_control/voice_control.tsx
+++ frontend/src/components/voice_control/voice_control.tsx
@@ -1,13 +1,37 @@
 import { MicrophoneIcon } from '@heroicons/react/24/outline';
-// import { useState } from 'react';
-// import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import { useEffect } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import "./voice_control.css";
 
 
+
 export default function VoiceControl() {
+    const { transcript, listening, resetTranscript } = useSpeechRecognition();
+
+
+    useEffect(() => {
+        if (!listening && transcript) {
+            fetch('http://127.0.0.1:5000/command', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    command: transcript,
+                })
+            }).catch(error => alert('Error: ' + error.message));
+
+            resetTranscript();
+        }
+    }, [listening, transcript, resetTranscript]);
+
+    const handleClick = () => {
+        SpeechRecognition.startListening({ continuous: false });
+    };
+
     return (
-        <div className="bubble">
-            <div className="icon-box"><MicrophoneIcon /></div>
+        <div className={`bubble ${listening ? 'listening' : ''}`}>
+            <div className="icon-box" onClick={() => { handleClick() }}><MicrophoneIcon /></div>
         </div>
     );
 }
